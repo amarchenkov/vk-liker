4fafbab774c4f72344a021aa6cfc9dae953408bf
Andrey M <a.marchenkov@outlook.com>
2016-09-06 00:29:06 +0300

    liker service (nit finished)

--- vk-liker/src/main/java/com/github/vk/liker/source/impl/SearchSource.java
+++ vk-liker/src/main/java/com/github/vk/liker/source/impl/SearchSource.java
@@ -1,6 +1,12 @@
 package com.github.vk.liker.source.impl;
 
+import com.github.vk.api.VK;
+import com.github.vk.liker.jmx.GroupSourceMBean;
 import com.github.vk.liker.jmx.SearchSourceMBean;
+import com.github.vk.liker.service.LikeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedOperationParameter;
@@ -18,10 +24,44 @@ import java.util.Map;
 @ManagedResource(objectName = "Liker:name=SearchSource")
 public class SearchSource implements SearchSourceMBean {
 
+    private static final Logger LOG = LogManager.getLogger(GroupSourceMBean.class);
+
+    private LikeService likeService;
+    private int fromAge;
+    private int toAge;
+    private String city;
+    private String country;
+
+    @ManagedAttribute
+    public int getFromAge() {
+        return fromAge;
+    }
+
+    @ManagedAttribute
+    public int getToAge() {
+        return toAge;
+    }
+
+    @ManagedAttribute
+    public String getCity() {
+        return city;
+    }
+
+    @ManagedAttribute
+    public String getCountry() {
+        return country;
+    }
+
+    @Autowired
+    public void setLikeService(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+
     @Override
     @ManagedOperation(description = "Start liking search result")
     public void start() {
-
+        VK vk = new VK();
     }
 
 }
--- vk-liker/src/main/resources/liker.sh
+++ vk-liker/src/main/resources/liker.sh
@@ -1,1 +1,2 @@
 #!/usr/bin/env bash
+java -jar liker.jar -Dwebdriver.gecko.driver=E:\geckodriver.exe
