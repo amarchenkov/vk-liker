24c50cba7190faf51b9734864fe192508d8a31fa
AMarchenkov <andrey.marchenkov@firstlinesoftware.com>
2016-10-14 18:33:34 +0300

    proxy example

--- vk-api/src/test/java/com/github/vk/api/Test.java
+++ vk-api/src/test/java/com/github/vk/api/Test.java
@@ -8,8 +8,13 @@ import com.github.vk.api.models.json.LikesAddResponse;
 import com.github.vk.api.models.json.PhotosGetAllResponse;
 import com.github.vk.api.models.json.WallGetResponse;
 import org.junit.Ignore;
-
-import java.net.URL;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created at 16.08.2016 23:47
@@ -36,4 +41,25 @@ public class Test {
         System.out.println(vk.isLiked(ObjectType.POST, 36128013, 556));
     }
 
+
+    @org.junit.Test
+    public void test1() throws InterruptedException {
+        String PROXY = "94.177.170.238:" + 8080;
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy(PROXY);
+        proxy.setSslProxy(PROXY);
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
+        capabilities.setPlatform(Platform.LINUX);
+        capabilities.setBrowserName("chrome");
+
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
+        capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+
+        WebDriver driver = new FirefoxDriver(capabilities);
+        driver.get("http://2ip.ru");
+        Thread.sleep(50000);
+    }
 }
